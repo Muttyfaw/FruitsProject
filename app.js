7dbbbef04e6afec43240ff82fa112e0a6732f768
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-17 17:04:54 +0000

    on it

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-const { MongoClient } = require("mongodb");
+const  MongoClient  = require("mongodb");
+const assert = require("assert")
 
 // Replace the uri string with your connection string.
-const url = "<connection string uri>";
+const url = "mongodb://localhost:27017";
 
-const client = new MongoClient(uri);
+// Database name
+const dbName = "fruitsDB"
 
-async function run() {
-    try {
-        const database = client.db('sample_mflix');
-        const movies = database.collection('movies');
+//New mongo client
+const client = new MongoClient(url);
 
-        // Query for a movie that has the title 'Back to the Future'
-        const query = { title: 'Back to the Future' };
-        const movie = await movies.findOne(query);
+//Use connct method to connect to the server
+client.connect(function(err){
+    assert.equal(null, err)
+    console.log("Connected successfully to server")
 
-        console.log(movie);
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
+    const db = client.db(dbName)
+
+    client.close()
+})
+
+// async function run() {
+//     try {
+//         const database = client.db('sample_mflix');
+//         const movies = database.collection('movies');
+
+//         // Query for a movie that has the title 'Back to the Future'
+//         const query = { title: 'Back to the Future' };
+//         const movie = await movies.findOne(query);
+
+//         console.log(movie);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+// run().catch(console.dir);
