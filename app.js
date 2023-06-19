0f3fd198fc2cc91b4b74ebe2178e612c06331a1a
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-19 15:30:27 +0000

    getting there

--- app.js
+++ app.js
@@ -1,38 +1,55 @@
-const  MongoClient  = require("mongodb").MongoClient;
-const assert = require("assert")
+// Importing MongoClient from mongodb driver
+const { MongoClient } = require('mongodb');
 
-// Replace the uri string with your connection string.
-const url = "mongodb://127.0.0.1:27017";
+// Connecting to a local port
+const uri = 'mongodb://127.0.0.1:27017';
 
-// Database name
-const dbName = "fruitsDB"
+const client = new MongoClient(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 
-//New mongo client
-const client = new MongoClient(url, { useNewUrlParser: true });
+connect();
 
-//Use connct method to connect to the server
-client.connect(function(err){
-    assert.equal(null, err)
-    console.log("Connected successfully to server")
+// ESNext syntax using async-await
+async function connect() {
+    try {
+        await client.connect();
+        const db = client.db('fruitsDB');
+        console.log(
+            `Successfully connected to db ${db.databaseName}`);
+        
+        const fruits = db.collection('fruits');
 
-    const db = client.db(dbName)
+        // Insertion
+        const cursorInsertion = await fruits.insertMany([
+            {
+                name : "Apple",
+                score: 8 ,
+                review : 'Great Fruit'
+            },
+            {
+                name : "Orange",
+                score : 6 ,
+                review : "Kinda sour"
+            },
+            {
+                name: "Banana",
+                score: 9 ,
+                review: "Great stuff"
+            }
+        ]);
+        console.log(cursorInsertion.insertedCount);
 
-    client.close()
-})
-
-// async function run() {
-//     try {
-//         const database = client.db('sample_mflix');
-//         const movies = database.collection('movies');
-
-//         // Query for a movie that has the title 'Back to the Future'
-//         const query = { title: 'Back to the Future' };
-//         const movie = await movies.findOne(query);
-
-//         console.log(movie);
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         await client.close();
-//     }
-// }
-// run().catch(console.dir);
+        // Display
+        const cursorFind = fruits.find();
+        const data = await cursorFind.toArray();
+        console.table(data);
+    }
+    catch (err) {
+        console.error(`we encountered ${err}`);
+    }
+    finally {
+        client.close();
+    }
+}
