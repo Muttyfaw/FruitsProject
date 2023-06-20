489547fa85079a7dc2f2612166a05c91e7d76d4c
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-20 10:19:51 +0000

    I'm getting there

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // Importing MongoClient from mongodb driver
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/fruitsDB")
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB")
+// console.log("Running")
+// const {Schema, model} = mongoose
+
 
 const fruitSchema = new mongoose.Schema({
     name: String,
@@ -17,7 +20,22 @@ const fruit = new Fruit ({
     review: "Pretty solid as a fruit"
 })
 
-fruit.save()
+const PersonSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+
+const Person = mongoose.model("Person", PersonSchema)
+
+const person = new Person ({
+    name: "John",
+    age: 34
+})
+
+console.log(person)
+
+people
+
 
    
        
